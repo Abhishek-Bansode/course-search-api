b465f3f63187b863d3292027bf7f72bc18980bba
Abhishek Bansode <88792504+Abhishek-Bansode@users.noreply.github.com>
2025-07-12 23:10:42 +0530

    define course document structure and docker-compose file

---
+++ src/main/java/com/abhishek/coursesearch/model/CourseDocument.java
@@ -0,0 +1,51 @@
+package com.abhishek.coursesearch.model;
+
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(indexName = "courses")
+public class CourseDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Keyword)
+    private String type; // ONE_TIME, COURSE, CLUB
+
+    @Field(type = FieldType.Keyword)
+    private String gradeRange; // e.g., "1st–3rd"
+
+    @Field(type = FieldType.Integer)
+    private int minAge;
+
+    @Field(type = FieldType.Integer)
+    private int maxAge;
+
+    @Field(type = FieldType.Double)
+    private double price;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
+    private Instant nextSessionDate;
+}
